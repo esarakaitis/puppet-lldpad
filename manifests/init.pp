d83f5368535cd1a3393bea3bbe8e152a0bb8bb92
esarakaitis <eric@sarakaitis.com>
2014-07-22 10:18:24 -0400

    added configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,8 +8,23 @@
 # Sample Usage:
 #  class { 'lldpad': }
 #
-class git {
+class lldpad {
   package { 'lldpad':
     ensure => installed,
   }
 }
+
+exec 	{ 'lldptool adminstatus eth0':
+	command => 'lldptool -L -i eth0 adminstatus=rxtx',
+	}
+
+exec	{ 'lldptool adminstatus ncb eth0':
+	command => 'lldptool -i eth0 -g ncb -L adminstatus=rxtx',
+	}
+exec    { 'lldptool adminstatus eth1':
+        command => 'lldptool -L -i eth1 adminstatus=rxtx',
+        }
+
+exec    { 'lldptool adminstatus ncb eth1':
+        command => 'lldptool -i eth1 -g ncb -L adminstatus=rxtx',
+        }
