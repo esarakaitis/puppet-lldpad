c6dbc7c712c000fa64386d0ceb16857ee40ba942
esarakaitis <eric@sarakaitis.com>
2014-07-22 10:50:42 -0400

    added configuration files, services, etc

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,19 +12,26 @@ class lldpad {
   package { 'lldpad':
     ensure => installed,
   }
-}
 
-exec 	{ 'lldptool adminstatus eth0':
-	command => "lldptool -L -i eth0 adminstatus=rxtx",
-	}
 
-exec	{ 'lldptool adminstatus ncb eth0':
-	command => "lldptool -i eth0 -g ncb -L adminstatus=rxtx",
-	}
+service { 'lldpad':
+        ensure => running,
+        enable => true,
+        }
+
+exec    { 'lldptool adminstatus eth0':
+        command => "/usr/sbin/lldptool -L -i eth0 adminstatus=rxtx",
+        }
+
+exec    { 'lldptool adminstatus ncb eth0':
+        command => "/usr/sbin/lldptool -i eth0 -g ncb -L adminstatus=rxtx",
+        }
+
 exec    { 'lldptool adminstatus eth1':
-        command => "lldptool -L -i eth1 adminstatus=rxtx",
+        command => "/usr/sbin/lldptool -L -i eth1 adminstatus=rxtx",
         }
 
 exec    { 'lldptool adminstatus ncb eth1':
-        command => "lldptool -i eth1 -g ncb -L adminstatus=rxtx",
+        command => "/usr/sbin/lldptool -i eth1 -g ncb -L adminstatus=rxtx",
         }
+}
